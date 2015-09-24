24b6c8171c19c5f9faed13bed5fc3921910c5e68
Gustavo Preciado <glpreciadoj@unal.edu.co>
2015-09-24 04:05:15 -0500

    Seeds para crear dos mapas de nivel easy

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,5 @@
 class Map < ActiveRecord::Base
 	has_and_belongs_to_many :teams
+	serialize :board
+	LEVEL = {easy: 2, hard: 5}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,91 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+mapa_easy_1 = Map.new
+mapa_easy_1.name = "Easy 1"
+mapa_easy_1.level = Map::LEVEL[:easy]
+board = []
+board << {x:0,y:0,value:5}
+board << {x:1,y:0,value:3}
+board << {x:2,y:0,value:4}
+board << {x:3,y:0,value:1}
+board << {x:4,y:0,value:3}
+board << {x:5,y:0,value:'start'}
+board << {x:0,y:1,value:3}
+board << {x:1,y:1,value:1}
+board << {x:2,y:1,value:5}
+board << {x:3,y:1,value:3}
+board << {x:4,y:1,value:4}
+board << {x:5,y:1,value:2}
+board << {x:0,y:2,value:5}
+board << {x:1,y:2,value:1}
+board << {x:2,y:2,value:2}
+board << {x:3,y:2,value:2}
+board << {x:4,y:2,value:4}
+board << {x:5,y:2,value:1}
+board << {x:0,y:3,value:5}
+board << {x:1,y:3,value:3}
+board << {x:2,y:3,value:5}
+board << {x:3,y:3,value:1}
+board << {x:4,y:3,value:5}
+board << {x:5,y:3,value:4}
+board << {x:0,y:4,value:5}
+board << {x:1,y:4,value:3}
+board << {x:2,y:4,value:1}
+board << {x:3,y:4,value:2}
+board << {x:4,y:4,value:1}
+board << {x:5,y:4,value:1}
+board << {x:0,y:5,value:'target'}
+board << {x:1,y:5,value:3}
+board << {x:2,y:5,value:2}
+board << {x:3,y:5,value:2}
+board << {x:4,y:5,value:3}
+board << {x:5,y:5,value:4}
+mapa_easy_1.board = board
+mapa_easy_1.save
+
+
+
+mapa_easy_2 = Map.new
+mapa_easy_2.name = "Easy 2"
+mapa_easy_2.level = Map::LEVEL[:easy]
+board = []
+board << {x:0,y:0,value:4}
+board << {x:1,y:0,value:1}
+board << {x:2,y:0,value:5}
+board << {x:3,y:0,value:1}
+board << {x:4,y:0,value:1}
+board << {x:5,y:0,value:'start'}
+board << {x:0,y:1,value:1}
+board << {x:1,y:1,value:2}
+board << {x:2,y:1,value:1}
+board << {x:3,y:1,value:1}
+board << {x:4,y:1,value:5}
+board << {x:5,y:1,value:2}
+board << {x:0,y:2,value:1}
+board << {x:1,y:2,value:5}
+board << {x:2,y:2,value:2}
+board << {x:3,y:2,value:5}
+board << {x:4,y:2,value:2}
+board << {x:5,y:2,value:4}
+board << {x:0,y:3,value:4}
+board << {x:1,y:3,value:2}
+board << {x:2,y:3,value:3}
+board << {x:3,y:3,value:1}
+board << {x:4,y:3,value:4}
+board << {x:5,y:3,value:5}
+board << {x:0,y:4,value:3}
+board << {x:1,y:4,value:3}
+board << {x:2,y:4,value:3}
+board << {x:3,y:4,value:2}
+board << {x:4,y:4,value:5}
+board << {x:5,y:4,value:4}
+board << {x:0,y:5,value:'target'}
+board << {x:1,y:5,value:3}
+board << {x:2,y:5,value:4}
+board << {x:3,y:5,value:4}
+board << {x:4,y:5,value:3}
+board << {x:5,y:5,value:5}
+mapa_easy_2.board = board
+mapa_easy_2.save
+
